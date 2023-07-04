f47519b0c0180b44d042fb595532374ebf1f1f70
mhaprean <marianhaprean@gmail.com>
2023-07-04 14:53:56 +0300

    fix runa gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Navigation from './components/Navigation';
 import RunaImage from './components/RunaImage';
 
 function App() {
-
   const [count, setCount] = useState(0);
 
   return (
     <>
-
       <Navigation />
 
-
-      <div className='container'>
+      <div className="container">
         page content here
-        <RunaImage runaNumber={36789251} />
+        <RunaImage runaNumber={163851} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
