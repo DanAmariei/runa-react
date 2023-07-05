8adafbfa215c382cf3a382083be117c75c199a3c
DanAmariei <amd2508@gmail.com>
2023-07-05 16:05:57 +0300

    RUNA IMAGE

--- src/helper/runes.ts
+++ src/helper/runes.ts
@@ -10,18 +10,100 @@ interface IRunaList {
 
 export const runes: IRunaList = {
   fehu: {
-    title: "FEHU",
-    image: "https://img.freepik.com/premium-vector/black-square-background-with-fehu-rune-magic-circle_477392-467.jpg?w=2000"
+    title: "Fehu",
+    image: "/pngs/FEHU.png", 
   },
   uruz: {
-    title: "URUZ",
-    image: "/svgs/2.svg"
+    title: "Uruz",
+    image: "/pngs/URUZ.png"
   },
   thurisaz: {
-    title: "THURISAZ"
+    title: "Thurisaz",
+    image: "/pngs/THURISAZ.png",
   },
   ansuz: {
-    title: "ANSUZ"
+    title: "Ansuz",
+    image: "/pngs/ANSUZ.png",
+  },
+  raido: {
+    title: "Raido",
+    image: "/pngs/RAIDO.png",
+  },
+  kenaz: {
+    title: "Kenaz",
+    image: "/pngs/KENAZ.png",
+  },
+  gebo: {
+    title: "Gebo",
+    image: "/pngs/GEBO.png",
+  },
+  wunjo: {
+    title: "Wunjo",
+    image: "/pngs/WUNJO.png",
+  },
+  hagalaz: {
+    title: "Hagalaz",
+    image: "/pngs/HAGALAZ.png",
+  },
+  nauthiz: {
+    title: "Nauthiz",
+    image: "/pngs/NAUTHIZ.png",
+  },
+  isa: {
+    title: "Isa",
+    image: "/pngs/ISA.png",
+  },
+  jera: {
+    title: "Jera",
+    image: "/pngs/JERA.png",
+  },
+  eihwaz: {
+    title: "Eihwaz",
+    image: "/pngs/EIHWAZ.png",
+  },
+  perthrc: {
+    title: "Perthrc",
+    image: "/pngs/PERTHRC.png",
+  },
+  algiz: {
+    title: "Algiz",
+    image: "/pngs/ALGIZ.png",
+  },
+  sowilo: {
+    title: "Sowilo",
+    image: "/pngs/SOWILO.png",
+  },
+  tiewaz: {
+    title: "Tiewaz",
+    image: "/pngs/TIEWAZ.png",
+  },
+  berkana: {
+    title: "Berkana",
+    image: "/pngs/BERKANA.png",
+  },
+  ehwaz: {
+    title: "Ehwaz",
+    image: "/pngs/EHWAZ.png",
+  },
+  mannaz: {
+    title: "Mannaz",
+    image: "/pngs/MANNAZ.png",
+  },
+  laguz: {
+    title: "Laguz",
+    image: "/pngs/LAGUZ.png",
+  },
+  inguz: {
+    title: "Inguz",
+    image: "/pngs/INGUZ.png",
+  },
+  dagaz: {
+    title: "Dagaz",
+    image: "/pngs/DAGAZ.png",
+  },
+  othala: {
+    title: "Othala",
+    image: "/pngs/OTHALA.png",
   },
 
 };
